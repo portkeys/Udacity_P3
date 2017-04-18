7891671f1bb7b7e7eab6926003043b0d7a2e8a09
Wen Yang <pam.wenyang@gmail.com>
2017-04-17 17:57:35 -0700

    Initial Commit

--- Data_Wrangling_Prep/CaseStudy_Quiz/Quiz2_Tag_Types.py
+++ Data_Wrangling_Prep/CaseStudy_Quiz/Quiz2_Tag_Types.py
@@ -38,3 +38,16 @@ def process_map(filename):
 
     return keys
 
+def test():
+    # You can use another testfile 'map.osm' to look at your solution
+    # Note that the assertion below will be incorrect then.
+    # Note as well that the test function here is only used in the Test Run;
+    # when you submit, your code will be checked against a different dataset.
+    keys = process_map('example.osm')
+    pprint.pprint(keys)
+    assert keys == {'lower': 5, 'lower_colon': 0, 'other': 1, 'problemchars': 1}
+
+
+if __name__ == "__main__":
+    test()
+
