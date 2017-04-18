b9d493fd911aba7d8737ede1a30d34a58e3401df
Wen Yang <pam.wenyang@gmail.com>
2017-04-17 17:56:38 -0700

    Initial Commit

--- Data_Wrangling_Prep/CaseStudy_Quiz/Quiz1_Iterparsing_xml.py
+++ Data_Wrangling_Prep/CaseStudy_Quiz/Quiz1_Iterparsing_xml.py
@@ -15,4 +15,20 @@ def count_tags(filename):
 	tagdict=DICT(Counter(taglist))
 	return tagdict
 
+def test():
 
+    tags = count_tags('example.osm')
+    pprint.pprint(tags)
+    assert tags == {'bounds': 1,
+                     'member': 3,
+                     'nd': 4,
+                     'node': 20,
+                     'osm': 1,
+                     'relation': 1,
+                     'tag': 7,
+                     'way': 1}
+
+    
+
+if __name__ == "__main__":
+    test()
